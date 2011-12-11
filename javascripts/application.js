df58f33d4679f1f318283258bcb0bfd2c0cedaf0
Gianpaulo Soares <jam_pow@hotmail.com>
2011-12-11 20:33:15 -0200

    menu descendo junto com scroll

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,5 +1,18 @@
 $(function(){
   
+  var nav    = $('nav');
+  var navTop = nav.position().top * 1;
+  $(window).scroll(function(){
+    var view = $('html, body');
+    var pageTop = view.position().top * 1;
+    if (navTop + pageTop < 0) {
+      nav.css({'position': 'fixed', 'top': 0});
+    } else {
+      nav.css({'position': 'relative'});
+    }
+    
+  });
+  
   $('nav a.scrollTo').click(function(){
     var target = $(this).attr('href');
     $('html, body').animate({'scrollTop' : $(target).position().top}, 750);
