2e7cf5e753f7183d671332d7f2335c3c9d270fcc
Gianpaulo Soares <jam_pow@hotmail.com>
2011-12-18 00:55:25 -0200

    adicionando album

--- javascripts/application.js
+++ javascripts/application.js
@@ -20,6 +20,7 @@ $(function(){
     return false;
   });
 
+  //QuickSand
   $('#filter li').click(function(){
     var value = $(this).attr('data-id');
     $('.portfolio').quicksand( $('.dic li.'+value), {
@@ -27,4 +28,43 @@ $(function(){
     });
   });
 
+});
+
+$(window).load(function(){
+  //Album
+  //$('#album li'      ).hide();
+  $('#album li:first').show().addClass('active');
+  $('#album').height($('#album li:first img').height());
+  
+  $('#control li').click(function(){
+    var id = $(this).attr('id');
+    var active = $('#album li.active');
+    var main = $('#album');
+
+    if (id == 'next') {
+      main.slideUp('normal', function(){
+        
+        if (active.next().length == 0) {
+          active.removeClass('active').hide();
+          $('#main li:first').addClass('active').show();
+        } else {
+          active.removeClass('active').hide().next().addClass('active').show();
+        }
+        main.css('height', 'auto').slideDown();
+        
+      });
+    } else {
+      main.slideUp('normal', function(){
+      
+        if (active.prev().length == 0) {
+          active.removeClass('active').hide();
+          $('#main li:last').addClass('active').show();
+        } else{
+          active.removeClass('active').hide().prev().addClass('active').show();
+        }
+        main.css('height', 'auto').slideDown();
+      });
+    }
+  });
+  
 });
